c17935af1b4b10f237a09f214fcd318692be739f
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-22 14:08:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/rk134/kernel_retarded_vince.git -b retarded-perf retard
 cd retard
-export ID=$CHANNEL_ID
-export BOT_API_KEY=$BOT_API_KEY
+export BOT_API_KEY=2055409010:AAGcPz6Uytme9ZQUL_b3LrlyUyl3xdtjgZ0
 bash build.sh 
