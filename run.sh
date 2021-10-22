7510a973a474a5359b496c4bc15f873e3aabaa39
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-22 20:38:23 +0100

    vince: add slmk [TEST]

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf retard && clang
 git clone https://github.com/rk134/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 clear
-git clone https://github.com/rk134/kernel_retarded_vince.git -b retarded-perf-test retard
+git clone https://github.com/rk134/kernel_retarded_vince.git -b retarded-test retard
 cd retard
 export ID=$ID
 export BOT_API_KEY=$BOT_API_KEY
