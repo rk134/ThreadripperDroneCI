f5dd771c1ef4a4c579b3ecdf0ff6cd4cd58d00dc
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-22 16:24:28 +0100

    add a script to fix compilation error

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+rm -rf retard && clang
+git clone https://github.com/akhilnarang/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 git clone https://github.com/rk134/kernel_retarded_vince.git -b retarded-perf retard
 cd retard
 export ID=$ID
 export BOT_API_KEY=$BOT_API_KEY
+make O=out mrproper
 bash build.sh 
