52f61030a2e86c8613f406d10c27d6031eb716d7
rahul <41164942+rk134@users.noreply.github.com>
2021-12-22 08:41:31 +0000

    upstream right branch

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf epyc && clang
 git clone https://github.com/rk134/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 clear
-git clone https://github.com/rk134/kernel_threadripper_vince.git -b threadripper-lmk epyc
+git clone https://github.com/rk134/kernel_threadripper_vince.git -b lmk-backup epyc
 cd epyc
 export ID=$ID
 export BOT_API_KEY=$BOT_API_KEY
