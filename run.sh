1ba3306ae70c894e15fb6b7dbf5272f271371321
rahul <rahul-k@bigdi.cc>
2022-05-17 07:46:33 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 git clone https://github.com/rk134/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 clear
-git clone https://github.com/rk134/kernel_xiaomi_vince -b r12.1 kernel
+git clone https://github.com/rk134/kernel_xiaomi_vince -b gcc-lto-fixing kernel
 cd kernel
-export CHANNEL_ID=$CHANNEL_ID
-export TELEGRAM_TOKEN=$TELEGRAM_TOKEN
-make O=out vince-perf_defconfig
-bash make-kernel.sh
+make O=out vince-perf_defconfig && export CHANNEL_ID=$CHANNEL_ID && export TELEGRAM_TOKEN=$TELEGRAM_TOKEN && bash make-kernel.sh
  
