566ac56927e23593073154d62593f423e4ea8a24
rahul <rahul-k@bigdi.cc>
2022-05-17 17:29:39 +0100

    [skip ci]

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-sudo apt install git
 git clone https://github.com/akhilnarang/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 clear
 git clone https://github.com/rk134/kernel_xiaomi_vince -b gcc-lto-fixing kernel
