73822132f9696bd5a17ce171c90eb6bf6cc168e3
rahul <rahul-k@bigdi.cc>
2022-05-17 17:24:36 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/rk134/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
+git clone https://github.com/akhilnarang/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
 clear
 git clone https://github.com/rk134/kernel_xiaomi_vince -b gcc-lto-fixing kernel
 cd kernel
